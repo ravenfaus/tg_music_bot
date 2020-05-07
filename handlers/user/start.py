f7fbc9403288469e90a902b8a1a1348502db1158
RavenFaus <ravenfaus@gmail.com>
2020-05-07 23:27:01 +0300

    Edited start message

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,10 +1,13 @@
 from aiogram import types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from models.user import User
 
 
 async def start_message(message: types.Message):
     text = f'Добро пожаловать, {message.from_user.full_name}!\n' \
-           f'Чтобы начать искать песни, введи исполнителя и/или название песни. Я постараюсь найти эту песню:)'
-    await message.answer(text)
+           f'Чтобы начать искать песни, введи исполнителя и/или название песни. Я постараюсь найти эту песню.\n' \
+           f'Также я могу искать песни в inline режиме. Нажми на кнопку, чтобы попробовать. Надеюсь понравится:)'
+    kb = InlineKeyboardMarkup().add(InlineKeyboardButton('Попробовать', switch_inline_query_current_chat=''))
+    await message.answer(text, reply_markup=kb)
 
